f6da42c3af500d3f1c64a2cc8101ec1848e63788
karthikeyanselvam1 <karthikeyanselvam@gmail.com>
2020-10-10 12:08:48 +0530

    CIDevOps Project

---
+++ CIDevOps/src/HadndlingCIdevOps.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HadndlingCIdevOps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Meenatchi\\Desktop\\Java class Videos\\chromedriver_win32_85\\chromedriver.exe");
+			
+			WebDriver driver = new ChromeDriver();
+			driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2fTestComplete11%2fWebOrders%2fProcess.aspx");
+
+			System.out.println(driver.getTitle());
+			
+			WebElement username = driver.findElement(By.xpath("//*[@id='ctl00_MainContent_username']"));
+			username.sendKeys("Tester");
+
+			WebElement password = driver.findElement(By.xpath("//*[@id='ctl00_MainContent_password']"));
+			password.sendKeys("test");
+			
+			WebElement login = driver.findElement(By.xpath("//*[@id='ctl00_MainContent_login_button']"));
+			login.click();
+
+
+	}
+
+}
